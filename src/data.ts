26b3c10531811d97502836662602ea34f2f881c4
Daniel Amado <daniel.amadove@gmail.com>
2025-03-13 15:29:56 -0500

    Add reference section and data for women's health profiles

---
+++ src/data.ts
@@ -0,0 +1,66 @@
+const perfilesMujeres = [
+  {
+    id: 1,
+    img: '/bianca',
+    nombre: 'Bianca',
+    edad: '29 años',
+    text: 'Ciclos irregulares, dismenorrea severa.',
+    AOC: 'Bellaface'
+  },
+  {
+    id: 2,
+    img: '/andrea',
+    nombre: 'Andrea',
+    edad: '22 años',
+    text: 'Deportista, teme que su anticonceptivo afecte su peso corporal.',
+    AOC: 'MIA'
+  },
+  {
+    id: 3,
+    img: '/manuela',
+    nombre: 'Manuela',
+    edad: '17 años',
+    text: 'Necesidad anticonceptiva, acné y seborrea moderados.',
+    AOC: 'Bellaface'
+  },
+  {
+    id: 4,
+    img: '/paola',
+    nombre: 'Paola',
+    edad: '40 años',
+    text: 'Síntomas físicos del síndrome premenstrual.',
+    AOC: 'MIA20'
+  },
+  {
+    id: 5,
+    img: '/monica',
+    nombre: 'Mónica',
+    edad: '33 años',
+    text: 'Síntomas emocionales relacionados con el ciclo menstrual.',
+    AOC: 'MIA20'
+  },
+  {
+    id: 6,
+    img: '/alejandra',
+    nombre: 'Alejandra',
+    edad: '38 años',
+    text: 'Dismenorrea y sangrado menstrual abundante, sedentaria.',
+    AOC: 'Bellaface20'
+  },
+  {
+    id: 7,
+    img: '/diana',
+    nombre: 'Diana',
+    edad: '19 años',
+    text: 'Se siente hinchada y pesada cerca de su período menstrual, acné hormonal.',
+    AOC: 'MIA'
+  },
+  {
+    id: 8,
+    img: '/emilia',
+    nombre: 'Emilia',
+    edad: '35 años',
+    text: 'Fumadora (menos de 15 cigarrillos al día), sangrados menstruales prolongados.',
+    AOC: 'Bellaface20'
+  }
+];
